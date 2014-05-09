e1088c9efaba1046dc3ce0a66a53d3137da8af6d
James Smith <smith750@indiana.edu>
2014-05-09 17:19:32 -0400

    list both classes and fields within classes

--- src/ojbreader/core.clj
+++ src/ojbreader/core.clj
@@ -2,20 +2,29 @@
 (require '[clojure.xml :as xml])
 (require '[clojure.java.io :as io])
 
-(def ^:dynamic *BASE-KFS-DIR* "c:\\java\\projects\\release-5-cgb\\work\\src\\org\\kuali\\kfs\\")
+(def BASE-KFS-DIR "c:\\java\\projects\\release-5-cgb\\work\\src\\org\\kuali\\kfs\\")
 
 (defn read-ojb-file [ojb-file-name]
 	(let [parsed-xml (xml/parse (io/file ojb-file-name))]
 		(doseq 
 			[x (xml-seq parsed-xml)
 				:when (= :class-descriptor (:tag x))]
-			(println (str (:class (:attrs x)) ", " (:table (:attrs x))))
+			(println (str (:class (:attrs x)) ", " (:table (:attrs x)) ))
+			(doseq
+				[y (:content x)
+					:when (and 
+						(= :field-descriptor (:tag y)) 
+						(contains? (:attrs y) :primarykey) 
+						(= "true" (:primarykey (:attrs y)))
+							)]
+					(println (str "\t" (:name (:attrs y)) ", " (:column (:attrs y))) )
+			)
 		)
 	)
 )
 
 (defn -main [& args]
 	(do
-		(read-ojb-file (str *BASE-KFS-DIR* "sys\\ojb-sys.xml"))
+		(read-ojb-file (str BASE-KFS-DIR "sys\\ojb-sys.xml"))
 	)
 )
