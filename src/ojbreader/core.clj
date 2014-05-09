e3221deda4eba7b2f00c96355e375b3ec06d3af5
James Smith <smith750@indiana.edu>
2014-05-09 15:53:49 -0400

    start reading ojb file via xml-seq

--- src/ojbreader/core.clj
+++ src/ojbreader/core.clj
@@ -1,6 +1,21 @@
 (ns ojbreader.core)
+(require '[clojure.xml :as xml])
+(require '[clojure.java.io :as io])
+
+(def ^:dynamic *BASE-KFS-DIR* "c:\\java\\projects\\release-5-cgb\\work\\src\\org\\kuali\\kfs\\")
+
+(defn read-ojb-file [ojb-file-name]
+	(let [parsed-xml (xml/parse (io/file ojb-file-name))]
+		(doseq 
+			[x (xml-seq parsed-xml)
+				:when (= :class-descriptor (:tag x))]
+			(println (str (:class (:attrs x)) ", " (:table (:attrs x))))
+		)
+	)
+)
+
 (defn -main [& args]
 	(do
-		(println "hello world")
+		(read-ojb-file (str *BASE-KFS-DIR* "sys\\ojb-sys.xml"))
 	)
 )
